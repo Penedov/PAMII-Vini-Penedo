ae9bd6d17773cb4e336aad4dddf3aea0348d01b1
Vinicius Penedo <Viniciuspenedo2003@gmail.com>
2025-03-21 22:26:22 -0300

    Aula sobre firebase: configurando firebase

---
+++ Aula_04/Aula_04/app/index.tsx
@@ -0,0 +1,14 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBD6cKkk2vMApWEHdl0rSisvbQKa8u34Ds",
+  authDomain: "meu-primeiro-projeto-4f6c8.firebaseapp.com",
+  projectId: "meu-primeiro-projeto-4f6c8",
+  storageBucket: "meu-primeiro-projeto-4f6c8.firebasestorage.app",
+  messagingSenderId: "773836567228",
+  appId: "1:773836567228:web:af788262cc0312a46596b9"
+};
+
+firebase.initializeApp(firebaseConfig);
+
